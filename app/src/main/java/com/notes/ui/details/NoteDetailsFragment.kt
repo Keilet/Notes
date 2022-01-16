ea68f17b267ce7bb3c48e08a6572344ad2b8f7bc
Keilet <6katika6@gmail.com>
2022-01-16 03:46:44 +0200

    Create onBaskPressed() on toolbar NoteDetailsFragment

--- app/src/main/java/com/notes/ui/details/NoteDetailsFragment.kt
+++ app/src/main/java/com/notes/ui/details/NoteDetailsFragment.kt
@@ -1,11 +1,22 @@
 package com.notes.ui.details
 
+import android.os.Bundle
 import com.notes.databinding.FragmentNoteDetailsBinding
+import com.notes.ui.RootActivity
 import com.notes.ui._base.ViewBindingFragment
 
 class NoteDetailsFragment : ViewBindingFragment<FragmentNoteDetailsBinding>(
     FragmentNoteDetailsBinding::inflate
 ) {
+    override fun onViewBindingCreated(
+        viewBinding: FragmentNoteDetailsBinding,
+        savedInstanceState: Bundle?
+    ) {
+        super.onViewBindingCreated(viewBinding, savedInstanceState)
 
+        viewBinding.toolbar.setNavigationOnClickListener {
+            (activity as RootActivity).onBackPressed()
+        }
 
+    }
 }
